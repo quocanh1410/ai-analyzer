1a5cfd5ef3c29419b4ce0f298180199291e228d8
quocanh1410 <laquocanh1410@gmail.com>
2026-05-15 18:02:57 +0700

    update

--- tele.py
+++ tele.py
@@ -1,20 +1,4 @@
-# import requests
 
-# BOT_TOKEN = "8631408938:AAGjG4eMXfbs93kcURoh-3VSIHgLt3AXDf0"
-# CHAT_ID = "6615540935"
-
-# url = f"https://api.telegram.org/bot8631408938:AAGjG4eMXfbs93kcURoh-3VSIHgLt3AXDf0/sendMessage"
-
-# r = requests.post(
-#     url,
-#     json={
-#         "chat_id": CHAT_ID,
-#         "text": "hello"
-#     }
-# )
-
-# print(r.status_code)
-# print(r.text)
 
 
 import os
